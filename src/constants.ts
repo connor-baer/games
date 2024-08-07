99f15dca5de49134c0eb57b0acc8f00321ebcb13
Connor Bär <github@connorbaer.com>
2024-08-07 10:05:23 +0200

    New branding

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,11 @@
+// App
+export const LOGO = '🔮';
+export const LOCALE = 'en-US';
+
 // Game
 export const NUMBER_OF_CARDS = 60;
 export const MIN_PLAYERS = 3;
 export const MAX_PLAYERS = 6;
+// TODO: Make configurable per game?
 export const POINTS_CORRECT = 20;
 export const POINTS_PER_TRICK = 10;
-
-// App
-export const LOGO = '🧙🏻';
-export const LOCALE = 'en-US';

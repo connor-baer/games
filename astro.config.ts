992024dcb006ec2dd30f27712a086c57214d5ecd
Connor Bär <connor-baer@users.noreply.github.com>
2025-04-08 10:19:13 +0200

    Add offline support (#14)
    * Implement service worker
    
    * Prerender Qwixx pages
    
    * Re-add manifest

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import svelte from '@astrojs/svelte';
 import vercel from '@astrojs/vercel/serverless';
 import { browserslistToTargets } from 'lightningcss';
 import browserslist from 'browserslist';
+import AstroPWA from '@vite-pwa/astro';
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,7 +21,22 @@ export default defineConfig({
     prefetchAll: true,
     defaultStrategy: 'hover',
   },
-  integrations: [svelte(), db()],
+  integrations: [
+    svelte(),
+    db(),
+    AstroPWA({
+      base: '/',
+      scope: '/',
+      workbox: {
+        navigateFallback: '/',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\/$/],
+      },
+    }),
+  ],
   vite: {
     css: {
       transformer: 'lightningcss',
